0c930a6ebffa7421269410f9541a0e23ab9207f1
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-05-16 22:58:54 +0100

    Created app's markup

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import Button from "../button/Button";
 import Display from "../display/Display";
 import ButtonPanel from "../buttonpanel/ButtonPanel";
 
 class App extends Component {
   render() {
-    return <div>Hello React</div>;
+    return (
+      <div className="calculator">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
   }
 }
 
