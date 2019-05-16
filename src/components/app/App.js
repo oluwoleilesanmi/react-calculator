7b7533ae29a805276c933a29304071f00142417f
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-05-16 19:41:11 +0100

    Created button-panel's markup

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Button from "../button/Button";
 import Display from "../display/Display";
+import ButtonPanel from "../buttonpanel/ButtonPanel";
 
 class App extends Component {
   render() {
