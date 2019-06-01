bfa4e2e21ef1ed4c820bf85031632051232eda0a
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-06-01 09:23:27 +0100

    Refactored let to const

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -62,15 +62,15 @@ const calculate = (stateData, btnName) => {
   return { total, next, operator, clear };
 };
 
-let opMatched = btn => {
+const opMatched = btn => {
   return ["x", "+", "-", "/"].includes(btn);
 };
 
-let numMatched = btn => {
+const numMatched = btn => {
   return ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(btn);
 };
 
-let concat = (stateChar, char) => {
+const concat = (stateChar, char) => {
   return stateChar === "" ? char : stateChar + char;
 };
 
